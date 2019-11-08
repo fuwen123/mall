9e60f4187e5e5f4c4b4a1cd6c59cfc8c97727d10
czk <314222151@qq.com>
2019-11-08 17:19:06 +0800

    初始化项目

--- shop/application/database.php
+++ shop/application/database.php
@@ -6,7 +6,7 @@ return [
 // 服务器地址
 'hostname'       => '127.0.0.1',
 // 数据库名
-'database'       => 'ds_mall',
+'database'       => 'fn_mall',
 // 用户名
 'username'       => 'root',
 // 密码
@@ -20,7 +20,7 @@ return [
 // 数据库编码默认采用utf8
 'charset'        => 'utf8',
 // 数据库表前缀
-'prefix'         => 'ds_',
+'prefix'         => 'fn',
 // 数据库调试模式
 'debug'          => true,
 // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
